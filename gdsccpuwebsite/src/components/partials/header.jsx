71de25cc665048e089cf36a42e92e98f163fba1e
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2022-12-07 00:00:06 +0800

    "edited"

--- gdsccpuwebsite/src/components/partials/header.jsx
+++ gdsccpuwebsite/src/components/partials/header.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Header;
-=======
 import React from "react";
 
 const Header  = () => {
@@ -22,4 +9,3 @@ const Header  = () => {
 };
 
 export default Header ;
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
