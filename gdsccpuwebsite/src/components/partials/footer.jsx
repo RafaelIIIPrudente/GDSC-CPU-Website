33ed49a6516f9022b4f37d3f65b3ee17cdf14061
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2022-12-07 00:00:41 +0800

    "edited"

--- gdsccpuwebsite/src/components/partials/footer.jsx
+++ gdsccpuwebsite/src/components/partials/footer.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import React from 'react'
-=======
-import React from "react";
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
 
 const Footer = () => {
   return (
     <div>
-<<<<<<< HEAD
       
     </div>
   )
 }
 
 export default Footer;
-=======
-
-    </div>
-  )
-};
-
-export default Footer;
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
--- gdsccpuwebsite/src/components/partials/socials.jsx
+++ gdsccpuwebsite/src/components/partials/socials.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
 import React from "react";
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
 
 const Socials = () => {
   return (
     <div>
-<<<<<<< HEAD
-      
-    </div>
-  )
-}
-
-export default Socials;
-=======
 
     </div>
   )
 };
 
 export default Socials;
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
