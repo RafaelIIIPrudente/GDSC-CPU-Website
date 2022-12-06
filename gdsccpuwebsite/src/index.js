d691948c57033faec1828f84c13d49a51c1d7ba6
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2022-12-07 00:20:26 +0800

    "Edited"

--- gdsccpuwebsite/src/index.js
+++ gdsccpuwebsite/src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
-import BrowserRouter from 'react-router-dom'
+// import BrowserRouter from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <BrowserRouter />
+    {/* <BrowserRouter /> */}
   </React.StrictMode>
 );
 
