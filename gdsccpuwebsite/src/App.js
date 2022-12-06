782a5facb924333971335f0f34647d948955a8fd
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2022-12-07 00:23:20 +0800

    Managed to make the test branch merge all changes

--- gdsccpuwebsite/src/App.js
+++ gdsccpuwebsite/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import NavBar from './components/partials/navbar';
 
 function App() {
@@ -10,19 +9,5 @@ function App() {
     );
   };
   
-=======
-import './App.css';
-import TeamsPage from './components/views/teamsPage';
-
-
-function App() {
-  return (
-    <>
-    <TeamsPage/>
-    </>
-  );
-};
-
->>>>>>> 89057f0d6df5adeacf77fbc1af4bf4d7c2a6577c
 export default App;
  
