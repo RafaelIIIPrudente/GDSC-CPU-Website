af6bb93ddd7e8517eb703e7d5b30ad4e37310547
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2022-12-29 23:18:40 +0800

    created model for user

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,48 @@
+import { DataTypes } from "sequelize";
+import database from "../config/database";
+
+const Users = database.define('users', {
+  uuid: {
+    type: DataTypes.STRING,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }, 
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [3, 100]
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+}, {
+  freezeTableName: true
+});
+
+export default Users;
+
