4cf356bf619a08c0b9efcb695311bf5e3d9f272b
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-03 01:44:51 +0800

    fixed the authRoute module

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-
+import { getUsers } from '../controllers/users';
+import { getUserById } from '../controllers/users';
+import { createUser } from '../controllers/users';
+import { updateUser } from '../controllers/users';
+import { deleteUser } from '../controllers/users';
 
 const router = express.Router();
 
+
+//routes 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
