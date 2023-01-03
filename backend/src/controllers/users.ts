ec30e82d7191169e58f983dcec5bdc2232ed2fdd
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 03:08:16 +0800

    fixed users module

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -3,17 +3,19 @@ import argon2  from "argon2";
 import { Request, Response } from "express";
 
 export const getUsers = async(req: Request, res: Response) => {
+
   try {
     const response = await Users.findAll({
       attributes:['id', 'name', 'email', 'role']
     });
-    res.status(200).json(response);
+    res.status(200).json("Succesfully get users!");
   } catch (error: any) {
     res.status(500).json({msg: error.message});
   }
 };
 
 export const getUserById = async(req: Request, res: Response) => {
+
   try {
     const response = await Users.findOne({
       attributes:['id', 'name', 'email', 'role'],
@@ -29,19 +31,21 @@ export const getUserById = async(req: Request, res: Response) => {
 
 export const createUser = async(req: Request, res: Response) => {
   const {name, email, password, confPassword, role} = req.body;
+  
   if(password !== confPassword) return res.status(400).json({msg: "Password does not match!"})
-  const hashPassword = await argon2.hash(password);
+  
+  const hashPassword: string = await argon2.hash(password);
 
   try {
     await Users.create({
-      name:name,
+      name:name,     
       email: email,
-      password: password,
+      password: hashPassword,
       role: role
     })
     res.status(201).json({msg: "Succesfully Registered!"})
-  } catch (error: any) {
-    res.status(400).json({msg: error.message});
+  } catch (err: any) {
+    res.status(400).json({msg: "Unsuccesful!"});
   }
 };
 
