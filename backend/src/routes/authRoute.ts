ad2b536207d4dd6d4447ea85d90703d8435d0673
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 03:08:33 +0800

    fixed authRoute module

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import { getUsers } from '../controllers/users';
-import { getUserById } from '../controllers/users';
-import { createUser } from '../controllers/users';
-import { updateUser } from '../controllers/users';
-import { deleteUser } from '../controllers/users';
+// import express from 'express';
+// import { getUsers } from '../controllers/users';
+// import { getUserById } from '../controllers/users';
+// import { createUser } from '../controllers/users';
+// import { updateUser } from '../controllers/users';
+// import { deleteUser } from '../controllers/users';
 
-const router = express.Router();
+// const router = express.Router();
 
 
-//routes 
-router.get('/users', getUsers);
-router.get('/users/:id', getUserById);
-router.post('/users', createUser);
-router.patch('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+// //routes 
+// router.get('/users', getUsers);
+// router.get('/users/:id', getUserById);
+// router.post('/users', createUser);
+// router.patch('/users/:id', updateUser);
+// router.delete('/users/:id', deleteUser);
 
-export default router;
+// export default router;
