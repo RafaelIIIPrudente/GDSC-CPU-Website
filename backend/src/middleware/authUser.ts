b0e890ca3667b001de98bed8344e0135dd5c1fdb
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 03:08:53 +0800

    fixed authUser module

--- backend/src/middleware/authUser.ts
+++ backend/src/middleware/authUser.ts
@@ -1,28 +1,28 @@
- import { NextFunction, Request, Response } from "express";
-import User from "../models/userModel";
+//  import { NextFunction, Request, Response } from "express";
+// import User from "../models/userModel";
 
-export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
-    if(!req.session.userId) {
-        return res.status(401).json({msg: "Please login your account"});
-    }
-    const user = await User.findOne({
-        where: {
-          uuid: req.session.userId
-        }
-      });
-      if(!Users) return res.status(404).json({msg: "User not found"});
-      req.userId = user.id;
-      req.role = user.role;
-      next();
-}
+// export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+//     if(!req.session.userId) {
+//         return res.status(401).json({msg: "Please login your account"});
+//     }
+//     const user = await User.findOne({
+//         where: {
+//           uuid: req.session.userId
+//         }
+//       });
+//       if(!user) return res.status(404).json({msg: "User not found"});
+//       req.userId = user.id;
+//       req.role = user.role;
+//       next();
+// }
 
-export const adminOnly = async (req: Request, res: Response, next: NextFunction) => {
-    const user = await User.findOne({
-        where: {
-          uuid: req.session.userId
-        }
-      });
-      if(!Users) return res.status(404).json({msg: "User not found"});
-      if (user.role !== "admin") return res.status(403).json({msg: "Access denied"});
-      next();
-}
+// export const adminOnly = async (req: Request, res: Response, next: NextFunction) => {
+//     const user = await User.findOne({
+//         where: {
+//           uuid: req.session.userId
+//         }
+//       });
+//       if(!user) return res.status(404).json({msg: "User not found"});
+//       if (user.role !== "admin") return res.status(403).json({msg: "Access denied"});
+//       next();
+// }
