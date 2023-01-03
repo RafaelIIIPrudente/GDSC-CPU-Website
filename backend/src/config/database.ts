a322fa98115218dfbc1b079b00f6e897dcbfb7bf
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 05:47:21 +0800

    fixed database module

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,7 +1,9 @@
-
+//imports 
 import { Sequelize } from "sequelize";
 import dbConfig from "./dbConfig";
 
+//An Object Relational Mapper performs functions like handling database records by representing the data as objects.
+//this is used for the connnection of database
 const database = new Sequelize(dbConfig.DATABASE, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
