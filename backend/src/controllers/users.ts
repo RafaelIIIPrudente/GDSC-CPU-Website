14a92f1b1080e15aba367e94babcfdd91ca179e1
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 05:48:32 +0800

    fixed user module with sir Kauffman

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,14 +1,16 @@
 import Users from "../models/userModel";
 import argon2  from "argon2";
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 
 export const getUsers = async(req: Request, res: Response) => {
 
   try {
-    const response = await Users.findAll({
-      attributes:['id', 'name', 'email', 'role']
+    var response = await Users.findAll({
+      attributes:['uuid','id', 'name', 'email', 'role']
+    }) .catch((e) => {
+      console.error(e.message); // "oh, no!"
     });
-    res.status(200).json("Succesfully get users!");
+    res.status(200).json(response);
   } catch (error: any) {
     res.status(500).json({msg: error.message});
   }
@@ -30,8 +32,9 @@ export const getUserById = async(req: Request, res: Response) => {
 };
 
 export const createUser = async(req: Request, res: Response) => {
+
   const {name, email, password, confPassword, role} = req.body;
-  
+
   if(password !== confPassword) return res.status(400).json({msg: "Password does not match!"})
   
   const hashPassword: string = await argon2.hash(password);
@@ -42,63 +45,79 @@ export const createUser = async(req: Request, res: Response) => {
       email: email,
       password: hashPassword,
       role: role
-    })
+    }) .catch((e) => {
+      console.error(e.message); // "oh, no!"
+    }) 
     res.status(201).json({msg: "Succesfully Registered!"})
   } catch (err: any) {
     res.status(400).json({msg: "Unsuccesful!"});
+  } 
+} ;
+
+export const updateUser = async (req: Request, res: Response) => {
+  var user = await Users.findOne({    
+    where: {
+      uuid: req.params.id
+    }
+  }) .catch((e) => {
+    console.error(e.message); // "oh, no!"
+  }) .then((user) => {
+    return user?.get({
+      plain:true
+    })
+  });
+
+  if(!Users) return res.status(404).json({msg: "User not found"})
+  const {name, email, password, confPassword, role} = req.body;
+  var hashPassword: string; 
+  if(password === "" || password === null) {
+    hashPassword = user.password
+  } else {
+    hashPassword = await argon2.hash(password);
+  }
+  if(password !== confPassword) return res.status(400).json({msg: "Password does not match!"})
+  try {
+    await Users.update({
+      name:name,
+      email: email,
+      password: password,
+      role: role
+    },{
+      where:{
+        id: user.id
+      }
+    })
+    res.status(200).json({msg: "User Updated"})
+  } catch (error: any) {
+    res.status(400).json({msg: error.message});
   }
 };
 
-export const updateUser = async (req: Request, res: Response) => {
-  // const user = await Users.findOne({    
-  //   where: {
-  //     uuid: req.params.id
-  //   }
-  // });
-  // if(!Users) return res.status(404).json({msg: "User not found"})
-  // const {name, email, password, confPassword, role} = req.body;
-  // var hashPassword: string; 
-  // if(password === "" || password === null) {
-  //   hashPassword = user.password
-  // } else {
-  //   hashPassword = await argon2.hash(password);
-  // }
-  // if(password !== confPassword) return res.status(400).json({msg: "Password does not match!"})
-  // try {
-  //   await Users.update({
-  //     name:name,
-  //     email: email,
-  //     password: password,
-  //     role: role
-  //   },{
-  //     where:{
-  //       id: user.id
-  //     }
-  //   })
-  //   res.status(200).json({msg: "User Updated"})
-  // } catch (error: any) {
-  //   res.status(400).json({msg: error.message});
-  // }
-};
-
 export const deleteUser = async(req: Request, res: Response) => {
-  // const user = await Users.findOne({
+  const user = await Users.findOne({
     
-  //   where: {
-  //     uuid: req.params.id
-  //   }
-  // });
-  // if(!Users) return res.status(404).json({msg: "User not found"});
-  // try {
-  //   await Users.destroy({
-  //     where:{
-  //       id: user.id
-  //     }
-  //   })
-  //   res.status(200).json({msg: "User Deleted"})
-  // } catch (error: any) {
-  //   res.status(400).json({msg: error.message});
-  // }
+    where: {
+      uuid: req.params.id
+    }
+  }) .catch((e) => {
+    console.error(e.message); // "oh, no!"
+  }) .then((user) => {
+    return user?.get({
+      plain:true
+    })
+  });;
+
+  if(!Users) return res.status(404).json({msg: "User not found"});
+  try {
+    await Users.destroy({
+      where:{
+        id: user.id
+      }
+    })
+    res.status(200).json({msg: "User Deleted"})
+  } catch (error: any) {
+    res.status(400).json({msg: error.message});
+  }
 };
 
 
