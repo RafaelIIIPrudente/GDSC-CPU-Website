3ac1502c0f4a42238fa9f3dd84c1ca255faf5ae9
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-04 15:20:05 +0800

    fixed the auth and authRoute modules
    Co-authored-by: Dave00913 <deybalivio09@gmail.com>
    Co-authored-by: LeradoPaulVincent <paulvincent.lerado-21@cpu.edu.ph>

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,40 +1,64 @@
-// import Users from "../models/userModel";
-// import argon2 from "argon2";
+import Users from "../models/userModel";
+import argon2 from "argon2";
+import { Request, Response } from "express";
 
-// export const Login = async(req: Request, res: Response) => {
-//     const user = await Users.findOne({
-//         where: {
-//           email: req.body.email
-//         }
-//       });
-//       if(!Users) return res.status(404).json({msg: "User not found"});
-//       const match = await argon2.verify(user.password, req.body.password);
-//       if(!match) return res.status(400).jason({msg: "Wrong Password"});
-//       req.session.userId = user.uuid;
-//       const uuid = user.uuid;
-//       const name = user.name;
-//       const email = user.email;
-//       const role = user.role;
-//       res.status(200).json({uuid, name, email, role});
-// }
+declare module 'express-session' {
+  export interface SessionData {
+    user: { [key: string]: any };
+    userId: number;
+  }
+}
 
-// export const Me = async(req: Request, res: Response) => {
-//     if(!req.session.userId) {
-//         return res.status(401).json({msg: "Please login your account"});
-//     }
-//     const user = await Users.findOne({
-//         attributes: ['uuid', 'name', 'email', 'role'],
-//         where: {
-//           uuid: req.session.userId
-//         }
-//       });
-//       if(!Users) return res.status(404).json({msg: "User not found"});
-//       res.status(200).json(user);
-// }
+export const Login = async(req: Request, res: Response) => {
+    const user = await Users.findOne({
+        where: {
+          email: req.body.email
+        }
+      }) .catch((e) => {
+        console.error(e.message); 
+      }) .then((user) => {
+        return user?.get({
+          plain:true
+        })
+      });
 
-// export const logOut = (req: Request, res: Response) => {
-//     req.session.destroy((err) => {
-//         if(err) return res.status(400).json({msg: "Can't Log Out"});
-//         res.status(200).json({msg: "You have log out"});
-//     });
-// }
+      if(!user) return res.status(404).json({msg: "User not found"});
+
+      const match = await argon2.verify(user.password, req.body.password);
+
+      if(!match) return res.status(400).json({msg: "Wrong Password"});
+      req.session.userId = user.uuid;
+      const uuid = user.uuid;
+      const name = user.name;
+      const email = user.email;
+      const role = user.role;
+      res.status(200).json({uuid, name, email, role});
+}
+
+export const Me = async(req: Request, res: Response) => {
+    if(!req.session.userId) {
+        return res.status(401).json({msg: "Please login your account"});
+    }
+
+    const user = await Users.findOne({
+        attributes: ['uuid', 'name', 'email', 'role'],
+        where: {
+          uuid: req.session.userId
+        }
+      }) .catch((e) => {
+        console.error(e.message); // "oh, no!"
+      }) .then((user) => {
+        return user?.get({
+          plain:true
+        })
+      });;
+      if(!user) return res.status(404).json({msg: "User not found"});
+      res.status(200).json(user);
+}
+
+export const logOut = (req: Request, res: Response) => {
+    req.session.destroy((err) => {
+        if(err) return res.status(400).json({msg: "Can't Log Out"});
+        res.status(200).json({msg: "You have logged out"});
+    });
+}
--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,13 +1,13 @@
-// import express from 'express';
-// import { Login, logOut, Me } from '../controllers/auth';
+import express from 'express';
+import { Login } from '../controllers/auth';
+import { logOut } from '../controllers/auth';
+import { Me } from '../controllers/auth';
 
+const router = express.Router();
 
-// // const router = express.Router();
+//routes 
+router.post('/login', Login);
+router.get('/me', Me);
+router.delete('/logout', logOut);
 
-
-// //routes 
-// router.get('/me', Me);
-// router.post('/login', Login);
-// router.delete('/logout', logOut);
-
-// // export default router;
+export default router;
