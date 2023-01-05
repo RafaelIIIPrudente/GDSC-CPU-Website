d15494448e60c67ce197801e504783981111e4d9
seannamo15 <seannamo15@gmail.com>
2023-01-06 03:16:38 +0800

    finalized the user module
    Co-authored-by: RafaelIIIPrudente <RafaelIIIPrudente@users.noreply.github.com>
    Co-authored-by: Lyzza Flores <liechee@users.noreply.github.com>
    Co-authored-by: Dave00913 <deybalivio09@gmail.com>
    Co-authored-by: LeradoPaulVincent <paulvincent.lerado-21@cpu.edu.ph>

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -11,7 +11,7 @@ const router = express.Router();
 //routes 
 router.get('/users',verifyUser, adminOnly, getUsers);
 router.get('/users/:id',verifyUser, adminOnly, getUserById);
-router.post('/users',verifyUser, adminOnly, createUser);
+router.post('/users', verifyUser, adminOnly,createUser);
 router.patch('/users/:id',verifyUser, adminOnly, updateUser);
 router.delete('/users/:id',verifyUser, adminOnly, deleteUser);
 
