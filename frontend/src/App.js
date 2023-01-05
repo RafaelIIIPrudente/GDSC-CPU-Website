0cf45f4b52cbf6c95fca25b0df0c802d54ddae71
seannamo15 <seannamo15@gmail.com>
2023-01-06 03:18:21 +0800

    edited the app module
    Co-authored-by: RafaelIIIPrudente <RafaelIIIPrudente@users.noreply.github.com>
    Co-authored-by: Lyzza Flores <liechee@users.noreply.github.com>
    Co-authored-by: Dave00913 <deybalivio09@gmail.com>
    Co-authored-by: LeradoPaulVincent <paulvincent.lerado-21@cpu.edu.ph>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Login from "./components/Login";
 import Users from "./pages/Users";
-import Products from "./pages/Products";
+import Events from "./pages/Events";
 import AddUser from "./pages/AddUser";
 import EditUser from "./pages/EditUser";
 import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
+import EditEvent from "./pages/EditEvent";
+import NewsLetterSignUp from "./pages/NewsLetterSignUp";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/newsletter-signup" element={<NewsLetterSignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/users" element={<Users />} />
           <Route path="/users/add" element={<AddUser />} />
           <Route path="/users/edit/:id" element={<EditUser />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/add" element={<AddProduct />} />
-          <Route path="/products/edit/:id" element={<EditProduct />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/events/add" element={<AddProduct />} />
+          <Route path="/event/edit/:id" element={<EditEvent />} />
         </Routes>
       </BrowserRouter>
     </div>
