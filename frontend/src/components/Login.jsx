8309b2fd71575e2f67b20e6faaf4db30d38d1188
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-01-12 18:44:14 +0800

    added

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -26,32 +26,32 @@ const Login = () => {
 
   return (
     <>
-<section className="hero is-fullheight is-fullwidth">
-     <div class="create-event-form">
-    <div class="greetings create-event-form">
-      <div class="navbar">
-        <div class="container event-form"  style={{alignItems: 'center'}}>
-          <form onSubmit={Auth}>
-          {isError && <p className="has-text-centered">{message}</p>}
-            <div class="form-group form-item title-admin-login" style={{alignItems: 'flex-end'}}>
-              <h1 class="temp-title" style={{fontSize: 45, color: 'black', fontWeight: 500}}>Admin Login</h1>
+      <section className="hero is-fullheight is-fullwidth">
+        <div class="create-event-form">
+          <div class="greetings create-event-form">
+            <div class="navbar">
+              <div class="container event-form" style={{ alignItems: 'center' }}>
+                <form onSubmit={Auth}>
+                  {isError && <p className="has-text-centered">{message}</p>}
+                  <div class="form-group form-item title-admin-login" style={{ alignItems: 'flex-end' }}>
+                    <h1 class="temp-title" style={{ fontSize: 45, color: 'black', fontWeight: 500 }}>Admin Login</h1>
+                  </div>
+                  <div class="form-group form-item">
+                    <label>Email</label>
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} class="form-control" placeholder="Enter email" required />
+                  </div>
+                  <div class="form-group form-item">
+                    <label>Password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} class="form-control" placeholder="Enter password" required />
+                  </div>
+                  <button type="submit" class="btn btn-primary form-item" style={{ marginTop: 20 }}>{isLoading ? "Loading..." : "Login"}</button>
+                </form>
+              </div>
             </div>
-            <div class="form-group form-item">
-              <label>Email</label>
-              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} class="form-control" placeholder="Enter email" required/>
-            </div>
-            <div class="form-group form-item">
-              <label>Password</label>
-              <input type="password"  value={password} onChange={(e) => setPassword(e.target.value)} class="form-control" placeholder="Enter password" required/>
-            </div>
-            <button type="submit" class="btn btn-primary form-item" style={{marginTop: 20}}>{isLoading ? "Loading..." : "Login"}</button>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-    </section>
-    </> 
+      </section>
+    </>
   );
 };
 
